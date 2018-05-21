7a43259021fb594197fbca31a092191ac68cbf96
jacobfriis <jacob.friis@simplecomplex.net>
2018-05-21 14:02:38 +0200

    - phpunit test helpers. - Release 1.4.

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -2,7 +2,7 @@
 /**
  * SimpleComplex PHP Utils
  * @link      https://github.com/simplecomplex/php-utils
- * @copyright Copyright (c) 2017 Jacob Friis Mathiasen
+ * @copyright Copyright (c) 2017-2018 Jacob Friis Mathiasen
  * @license   https://github.com/simplecomplex/php-utils/blob/master/LICENSE (MIT License)
  */
 declare(strict_types=1);
